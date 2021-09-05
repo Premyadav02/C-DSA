1b59dd6a2b6f17e26bbdfd45462d536ed83180b8
Prem Yadav <81822755+Premyadav02@users.noreply.github.com>
2021-09-05 13:55:29 +0530

    Update 4 fibo.cpp

--- 4 fibo.cpp
+++ 4 fibo.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    int n = 9, t1 = 0, t2 = 1, nextTerm = 0;
+    int n, t1 = 0, t2 = 1, nextTerm = 0;
 
-    // cout << "Enter the number of terms: ";
-    // cin >> n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
 
     cout << "Fibonacci Series: ";
 
